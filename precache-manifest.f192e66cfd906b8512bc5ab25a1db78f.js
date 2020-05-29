420d5fe84d66ee6acc7011d2b5fed2486e0b8032
MrN-Nabhani <nabhaninajm@gmail.com>
2020-05-29 14:08:47 +0300

    Updates

---
+++ precache-manifest.f192e66cfd906b8512bc5ab25a1db78f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30aa7ab8add78bd2d3c443289beb7316",
+    "url": "/CFDidTheySolveIt/index.html"
+  },
+  {
+    "revision": "5aae87528af21eea2588",
+    "url": "/CFDidTheySolveIt/static/css/main.54975497.chunk.css"
+  },
+  {
+    "revision": "79e8698beef4a9a9bf1b",
+    "url": "/CFDidTheySolveIt/static/js/2.9f55abed.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/CFDidTheySolveIt/static/js/2.9f55abed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aae87528af21eea2588",
+    "url": "/CFDidTheySolveIt/static/js/main.13bebf26.chunk.js"
+  },
+  {
+    "revision": "47c0cff756345598bab4",
+    "url": "/CFDidTheySolveIt/static/js/runtime-main.89d0a00c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CFDidTheySolveIt/precache-manifest.d5875ca8e762eb5cd611bd1b11625710.js"
+  "/CFDidTheySolveIt/precache-manifest.f192e66cfd906b8512bc5ab25a1db78f.js"
 );
 
 self.addEventListener('message', (event) => {
